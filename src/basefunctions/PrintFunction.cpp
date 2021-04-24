6734dd67f71bd7370059da832f36927761e0eb86
LoRy24 <lorytherock24tutorial@gmail.com>
2021-04-24 13:41:24 +0200

    Update PrintFunction.cpp

--- src/basefunctions/PrintFunction.cpp
+++ src/basefunctions/PrintFunction.cpp
@@ -5,8 +5,12 @@
 using namespace std;
 
 class PrintFunction: public BaseFunction {
-    public: 
-        void executeFunc(string s) {
+    public:
+        bool checkFunc(string s) {
             
+        }
+
+        void executeFunc(string s) {
+
         }
 };
